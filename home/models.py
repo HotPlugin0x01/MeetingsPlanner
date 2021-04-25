213dbc60ad0efe9894391c83aee0193a03df4246
Abdul Wassay <wassaysardar35@gmail.com>
2021-04-24 21:13:16 -0400

    Add and Register models
    Add model 'Rooms' in home app and 'Meetings' in meeting app.

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Rooms(models.Model):
+    room_name = models.CharField(max_length=50)
+    room_num = models.IntegerField(default=1)
+    floor_num = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.room_name}: Room {self.room_num} on {self.floor_num}'
+        
--- meetings/models.py
+++ meetings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import time
+from home.models import Rooms
 
 # Create your models here.
+class Meetings(models.Model):
+    name = models.CharField(max_length=150)
+    date = models.DateField()
+    start_time = models.TimeField(time(8))
+    duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} at {self.start_time} on {self.date}'
