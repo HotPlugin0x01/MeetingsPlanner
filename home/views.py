38380fc9e5cdcb79fe9f1ef7510d3a4c3d7223e4
Abdul Wassay <wassaysardar35@gmail.com>
2021-04-24 21:43:13 -0400

    Update home view and home.html
    Update homepage to show list of meetings

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from meetings.models import Meetings
 
 
 def home(request):
-    return render(request, 'home/home.html')
+    return render(request, 'home/home.html', {'meetings': Meetings.objects.all()})
